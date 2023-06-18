0ec794bfe2a1e12a8c5869ddf1e0ac10bb7eafec
1hyyang <gnayyh1@gmail.com>
2023-06-18 23:33:50 +0900

    jsp

--- 01JSP_basic/src/main/java/dto/PageDto.java
+++ 01JSP_basic/src/main/java/dto/PageDto.java
@@ -2,13 +2,13 @@ package dto;
 
 public class PageDto {
 	
-	int blockamount = 5; // 페이지 블록 크기
-	int startno; // 페이지 블록 시작 번호
-	int endno; // 페이지 블록 끝 번호	
-	int realendno; // 게시물의 끝 페이지 번호
+	int blockamount = 5; // 페이지 블록의 크기
+	int startno; // 페이지 블록의 시작 번호
+	int endno; // 페이지 블록의 끝 번호	
+	int realendno; // 전체 게시물의 끝 페이지 번호
 	boolean prev, next; // 이전, 다음 버튼 (true: 보여주기)		
-	int total; // 총 게시물 수	
-	Criteria criteria; // pageno페이지 번호, amount페이지당 게시물 수
+	int total; // 전체 게시물 수	
+	Criteria criteria; // pageno 페이지 번호, amount 페이지당 게시물 수
 	
 	public PageDto() {
 		// TODO Auto-generated constructor stub
@@ -19,15 +19,15 @@ public class PageDto {
 		this.total = total;
 		this.criteria = criteria;
 		
-		// 페이지 블록의 끝 번호 구하기
+		// 페이지 블록의 끝 번호
 		endno = (int)((Math.ceil(criteria.pageno/(blockamount*1.0)))*blockamount);
-		startno = endno-(blockamount-1);
-		
-		// 끝 페이지 번호
+		// 페이지 블록의 시작 번호
+		startno = endno-(blockamount-1);		
+		// 전체 게시물의 끝 페이지 번호
 		realendno = (int)(Math.ceil((total*1.0)/criteria.getAmount()));
 		
 		// 페이지 블록의 끝 번호를 설정
-		// 게시물의 끝 페이지보다 큰 경우 게시물의 끝 페이지로 설정
+		// 전체 게시물의 끝 페이지 번호보다 큰 경우 전체 게시물의 끝 페이지 번호로 설정
 		endno = endno>realendno?realendno:endno;
 		
 		// 앞으로 이동/뒤로 이동 설정
